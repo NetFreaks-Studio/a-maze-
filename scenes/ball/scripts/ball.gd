89be32642b6cac1166dd50f2f0d8c25293d3cf95
Bernardo António Borda d'Água <berna.agua@gmail.com>
2019-02-19 02:29:39 +0000

    wip

--- scenes/ball/scripts/ball.gd
+++ scenes/ball/scripts/ball.gd
@@ -13,7 +13,7 @@ const bounce_force = 0.225
 
 #contants to make the ball rotation by friction look smooth
 #---may-require-tweaks---
-const friction = 0.025
+const friction = 0.03125*2
 const friction_multiplier = 1
 
 var motion = Vector2(0,0)
@@ -22,26 +22,33 @@ var normal = Vector2(0,-1)
 var gravity_multiplier = 1
 var rotation_force = 0
 
-#processes bounces, sliding and free falling
+func _process(delta):
+	_rotate_sprite(delta)
+
 func _physics_process(delta):
 	_update_gravity()
 	motion = move_bounce_and_slide(motion, delta)
-	#print(motion)
-	for i in mbs_collisions:
-		var col_motion = (i.travel + i.remainder)/delta
-		motion = motion*lerp(bounce_force, 1, abs(col_motion.angle_to(-i.normal)/(PI/2)))
-		_update_sprite_rotation()
-	_rotate_sprite(delta)
-	print("frame")
+	if mbs_collisions:
+		for i in mbs_collisions:
+			var col_motion = (i.travel + i.remainder)/delta
+			_update_sprite_rotation()
+			motion = motion*lerp(bounce_force, 1, abs(col_motion.angle_to(-i.normal)/(PI/2)))
+	else:
+		rotation_force = rotation_force*0.99
+	if motion == Vector2(0,0):
+		rotation_force = 0
 
-func _move_bounce_and_slide_aux(vel_rel, slide_min):
-	var col = move_and_collide(vel_rel)
+func _move_bounce_and_slide_aux(vel_rel, slide_min, vel_rel_remainder):
+	var col = move_and_collide(vel_rel_remainder)
+	var result = vel_rel
 	if col:
 		mbs_collisions.append(col)
+		
 		var normal = col.normal
-		var remainder = col.remainder
 		var tangent = normal.tangent()
-		print(normal)
+		
+		var remainder = col.remainder
+		
 		var bounce = vel_rel.bounce(normal)
 		
 		var vertical_velocity_after_impact = normal.dot(bounce)
@@ -50,22 +57,20 @@ func _move_bounce_and_slide_aux(vel_rel, slide_min):
 		var remainder_bounce = remainder.bounce(normal)
 		
 		if abs(vertical_velocity_after_impact) >= slide_min:
-			_move_bounce_and_slide_aux(remainder_bounce, slide_min)
-			return bounce
+			result = _move_bounce_and_slide_aux(bounce, slide_min, remainder_bounce)
 		elif abs(horizontal_velocity_after_impact) > 0.0001:
 			var slide = tangent*horizontal_velocity_after_impact
 			var remainder_slide = tangent*tangent.dot(remainder_bounce)
-			_move_bounce_and_slide_aux(remainder_slide, slide_min)
-			return slide
+			result = _move_bounce_and_slide_aux(slide, slide_min, remainder_slide)
 		else:
-			return Vector2(0,0)
-	return vel_rel
+			result = Vector2(0,0)
+	return result
 
 func move_bounce_and_slide(vel, delta):
 	mbs_collisions = []
 	var vel_rel = vel*delta
-	var slide_min = 210*delta
-	return _move_bounce_and_slide_aux(vel_rel, slide_min)/delta
+	var slide_min = 50*delta
+	return _move_bounce_and_slide_aux(vel_rel, slide_min, vel_rel)/delta
 
 #rotates the ball and all its physics atributes. 
 #Desired effect for the end user: map is rotated, the ball keeps being affected by the gravity (falling down), as per usual
@@ -85,7 +90,7 @@ func _rotate_sprite(delta):
 #updates the values by which the sprite will be rotated by (friction)
 func _update_sprite_rotation():
 	if abs(normal.angle_to(motion)):
-		rotation_force = (normal.angle_to(motion)/abs(normal.angle_to(motion)))*(normal.rotated(PI/2).abs()*motion).length()*friction*friction_multiplier
+		rotation_force = (normal.angle_to(motion)/abs(normal.angle_to(motion)))*(normal.tangent()*motion).length()*friction*friction_multiplier
 		
 func test(vec):
 	move_and_collide(vec)
